a97345ae7967d7b146c88081bb935e92a3c86fff
Youngfellows <Youngfellows@163.com>
2022-07-11 14:40:39 +0800

    xxxx

---
+++ CodeStudy/ch11/example05/Sample.cpp
@@ -0,0 +1,26 @@
+#include "./include/Sample.h"
+
+void test1()
+{
+    cout << "test1():: ..." << endl;
+   
+    cout << endl;
+}
+
+void test2()
+{
+    cout << "test2():: ..." << endl;
+    cout << endl;
+}
+
+void test3()
+{
+    cout << "test3():: ..." << endl;
+    cout << endl;
+}
+
+void test4()
+{
+    cout << "test4():: ..." << endl;
+    cout << endl;
+}
