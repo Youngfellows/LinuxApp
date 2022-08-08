2b2d940777588b9d768a222a53d4bfc47a075464
Your Name <you@example.com>
2022-08-09 00:37:04 +0800

    xxx

--- CodeStudy/ch01/example02/Sample.cpp
+++ CodeStudy/ch01/example02/Sample.cpp
@@ -1,8 +1,22 @@
 #include "./include/Sample.h"
 
-void test1()
+/**
+ * @brief  获取命令行参数
+ * @brief  测试: ./obj/test_sample.out  aa bb cc
+ * @param arg
+ * @param args
+ */
+void test1(int arg, char *args[])
 {
     cout << "test1():: ..." << endl;
+    cout << "test1():: arg:" << arg << endl;
+    for (int i = 0; i < arg; i++)
+    {
+        // char *param = args[i];
+        char *param = *(args + i);
+        cout << "args[" << i << "]=" << param << endl;
+    }
+
     cout << endl;
 }
 
