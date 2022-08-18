c391b8baebe11d0da5b7e11dca336abf09a1d366
Youngfellows <Youngfellows@163.com>
2022-08-18 17:46:30 +0800

    xxxx

--- CodeStudy/ch03/example15/Sample.cpp
+++ CodeStudy/ch03/example15/Sample.cpp
@@ -1,8 +1,52 @@
 #include "./include/Sample.h"
 
+/**
+ * @brief 等待指定pid的子进程结束
+ *
+ */
 void test1()
 {
     cout << "test1():: ..." << endl;
+    int status;         //子进程结束时返回的退出状态
+    pid_t pids[CHILDS]; //子进程数组
+    pid_t waitPid = -1; //结束子进程的pid
+    for (int i = 0; i < CHILDS; i++)
+    {
+        pids[i] = fork(); //创建5个子进程
+        if (pids[i] == -1)
+        {
+            perror("fork failed");
+            exit(-1);
+        }
+        else if (pids[i] == 0)
+        {
+            printf("子进程pid=%d,ppid=%d\n", getpid(), getppid());
+            sleep(i); //休眠一下
+            exit(i);  //正常终止子进程
+        }
+    }
+    //等待某个子进程结束
+    waitPid = waitpid(pids[2], &status, 0); //等待pids[4]子进程结束
+    printf("%d,进程执行了,子进程%d结束的,状态是%d\n", getpid(), waitPid, status);
+
+    if (WIFEXITED(status)) //正常终止子进程
+    {
+        printf("正常终止子进程,子进程的退出状态为%d\n", WEXITSTATUS(status));
+    }
+    else
+    {
+        printf("异常终止子进程,子进程的退出状态为%d\n", WEXITSTATUS(status));
+    }
+
+    if (WIFSIGNALED(status)) //是否被某个信号终止子进程
+    {
+        printf("被%d信号终止子进程\n", WTERMSIG(status));
+    }
+    else
+    {
+        printf("不是被信号终止子进程\n");
+    }
+
     cout << endl;
 }
 
--- CodeStudy/ch03/example15/include/Global.h
+++ CodeStudy/ch03/example15/include/Global.h
@@ -4,7 +4,17 @@
 #include <iostream>
 #include <string>
 #include <memory>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <errno.h>
 
 using namespace std;
+
+//常量 - 子线程的数量
+const int CHILDS = 5;
 
 #endif
