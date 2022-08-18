0b5f8e95ccb4e062331eeb4941135942287cea05
Youngfellows <Youngfellows@163.com>
2022-08-18 14:50:12 +0800

    xxxx

--- CodeStudy/ch03/example05/Sample.cpp
+++ CodeStudy/ch03/example05/Sample.cpp
@@ -1,8 +1,37 @@
 #include "./include/Sample.h"
 
+int global = 0; //定义全局变量
+
+/**
+ * @brief vfork创建的父子进程共享数据空间
+ *
+ */
 void test1()
 {
     cout << "test1():: ..." << endl;
+    int var = 0; //局部变量
+    pid_t pid;   //进程PID
+    printf("进程创建前\n");
+    pid = vfork(); //创建子进程
+    if (pid < 0)
+    {
+        perror("vfork error");
+        // exit(0);
+    }
+    else if (pid == 0)
+    {
+        printf("子进程:%d\n", getpid());
+        global++;
+        var++;
+        _exit(0); //结束子进程
+    }
+    else
+    {
+        printf("父进程:%d\n", getpid());
+    }
+    sleep(3); //休眠3秒
+    printf("pid=%d,global=%d,var=%d\n", getpid(), global, var);
+    exit(0);
     cout << endl;
 }
 
--- CodeStudy/ch03/example05/include/Global.h
+++ CodeStudy/ch03/example05/include/Global.h
@@ -4,6 +4,12 @@
 #include <iostream>
 #include <string>
 #include <memory>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
 
 using namespace std;
 
