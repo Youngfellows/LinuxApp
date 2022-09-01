9d6432892d9672eabf021b1b20e50213824f497c
Your Name <you@example.com>
2022-09-01 15:53:41 +0800

    xxx

--- CodeStudy/ch05/example03/Sample.cpp
+++ CodeStudy/ch05/example03/Sample.cpp
@@ -1,8 +1,20 @@
 #include "./include/Sample.h"
 
+//主线程的线程标识符
+std::thread::id mainThreadId = std::this_thread::get_id(); //获取主线程ID
+
+/**
+ * @brief 1. 获取线程ID
+ *
+ */
 void test1()
 {
     cout << "test1():: ..." << endl;
+    std::thread::id tid = std::this_thread::get_id(); //获取线程ID
+    cout << "主线程Thread ID:" << tid << endl;
+    isMainThread();                     //主函数掉用普通函数
+    std::thread myThread(isMainThread); //创建一个线程对象
+    myThread.join();                    //等待子线程结束
     cout << endl;
 }
 
@@ -22,4 +34,23 @@ void test4()
 {
     cout << "test4():: ..." << endl;
     cout << endl;
+}
+
+/**
+ * @brief 线程回调函数
+ * @brief 打印是否是主线程
+ *
+ */
+void isMainThread()
+{
+    std::thread::id tid = std::this_thread::get_id(); //获取线程ID
+    //判断线程ID是否和主线程相同
+    if (mainThreadId == tid)
+    {
+        cout << "这个线程是主线程,Thread ID:" << tid << endl;
+    }
+    else
+    {
+        cout << "这个线程不是主线程,Thread ID:" << tid << endl;
+    }
 }
--- CodeStudy/ch05/example03/include/Sample.h
+++ CodeStudy/ch05/example03/include/Sample.h
@@ -8,4 +8,10 @@ void test2();
 void test3();
 void test4();
 
+/**
+ * @brief 线程回调函数
+ *
+ */
+void isMainThread();
+
 #endif
