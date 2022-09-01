db4059af37d7c8b0670ac97752216e570e139be3
Your Name <you@example.com>
2022-09-01 16:14:20 +0800

    xxx

--- CodeStudy/ch05/example05/Sample.cpp
+++ CodeStudy/ch05/example05/Sample.cpp
@@ -1,8 +1,19 @@
 #include "./include/Sample.h"
 
+/**
+ * @brief 让线程暂停5秒
+ *
+ */
 void test1()
 {
     cout << "test1():: ..." << endl;
+    cout << "倒计时开始:" << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << i << endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1)); //暂停1秒钟
+    }
+    cout << "倒计时结束:" << endl;
     cout << endl;
 }
 
--- CodeStudy/ch05/example05/include/Global.h
+++ CodeStudy/ch05/example05/include/Global.h
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <string>
 #include <memory>
+#include <chrono>
+#include <thread>
 
 using namespace std;
 
