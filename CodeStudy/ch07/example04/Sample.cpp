a559d0711e71ad5d61e959c7abdcb91da0078936
Your Name <you@example.com>
2022-09-14 17:27:59 +0800

    xxx

--- CodeStudy/ch07/example04/Sample.cpp
+++ CodeStudy/ch07/example04/Sample.cpp
@@ -1,9 +1,39 @@
 #include "./include/Sample.h"
 
+/**
+ * @brief 1. 调用abort函数发送SIGABRT信号使程序异常终止
+ *
+ */
 void test1()
 {
     cout << "test1():: ..." << endl;
+    if (signal(SIGABRT, signalHandler) == SIG_ERR)
+    {
+        printf("注册信号SIGABRT的信号处理函数错误\n");
+        exit(1); //结束进程
+    }
+    abort(); //使程序异常终止
+    printf("程序异常终止了,这句不会打印 ...\n");
     cout << endl;
+}
+
+/**
+ * @brief 信号处理函数
+ *
+ * @param sig 信号
+ */
+void signalHandler(int sig)
+{
+    printf("signalHandler begin ...\n");
+    switch (sig)
+    {
+    case SIGABRT:
+        printf("捕捉到信号: SIGABRT ...\n");
+        break;
+    default:
+        printf("捕捉到其他信号: %d\n", sig);
+        break;
+    }
 }
 
 void test2()
--- CodeStudy/ch07/example04/include/Global.h
+++ CodeStudy/ch07/example04/include/Global.h
@@ -4,6 +4,10 @@
 #include <iostream>
 #include <string>
 #include <memory>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <signal.h>
 
 using namespace std;
 
