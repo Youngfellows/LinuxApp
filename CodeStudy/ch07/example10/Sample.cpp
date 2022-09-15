fe07f8eda6ce167faaa4ed8a7fd77bb84f90c9cd
Your Name <you@example.com>
2022-09-15 15:44:09 +0800

    xxxx

--- CodeStudy/ch07/example10/Sample.cpp
+++ CodeStudy/ch07/example10/Sample.cpp
@@ -1,8 +1,32 @@
 #include "./include/Sample.h"
 
+/**
+ * @brief 1. 获得进程被屏蔽的信号
+ *
+ */
 void test1()
 {
     cout << "test1():: ..." << endl;
+    sigset_t oldSet;                                //信号集
+    int member;                                     //信号是否包含在信号集中
+    if (sigprocmask(SIG_BLOCK, NULL, &oldSet) == 0) //获取当前信号屏蔽子
+    {
+        //判断当前信号屏蔽字是否包含SIGINT
+        member = sigismember(&oldSet, SIGINT);
+        if (member == 1)
+        {
+            printf("当前信号屏蔽字包含信号:SIGINT\n");
+        }
+        else if (member == 0)
+        {
+            printf("当前信号屏蔽字不包含信号:SIGINT\n");
+        }
+        else
+        {
+            printf("调用sigismember错误\n");
+        }
+    }
+
     cout << endl;
 }
 
