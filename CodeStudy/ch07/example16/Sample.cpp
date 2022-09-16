8da063d793d245f0e24b28eb5b9e0cda774b3acf
Your Name <you@example.com>
2022-09-16 17:40:35 +0800

    xxxx

--- CodeStudy/ch07/example16/Sample.cpp
+++ CodeStudy/ch07/example16/Sample.cpp
@@ -1,9 +1,74 @@
 #include "./include/Sample.h"
 
+/**
+ * @brief 管道读出端关闭,写管道收到SIGPIPE信号
+ *
+ */
 void test1()
 {
     cout << "test1():: ..." << endl;
+    pid_t pid;  //进程ID
+    int fds[2]; //管道读出端,写入端
+    const char *data = "今天是周五,马上下班了,祝大家周末愉快 ...\n";
+    int size = strlen(data); //求字符串的大小
+    printf("data size:%d\n", size);
+    //设置当前进程对SIGCHLD信号的捕捉函数
+    if (signal(SIGCHLD, signalHandler) == SIG_ERR)
+    {
+        printf("设置当前进程对SIGCHLD信号的捕捉函数错误\n");
+        exit(0); //结束进程
+    }
+
+    if (pipe(fds) == 0) //创建管道
+    {
+        pid = fork(); //创建子进程
+        if (pid == 0) //子进程
+        {
+            pid_t self = getpid(); //获取进程ID
+            printf("子进程pid:%d\n", self);
+            printf("子进程等待2秒,等待父进程关闭管道读出端\n");
+            sleep(2);                          //等待2秒
+            close(fds[0]);                     //关闭子进程管道读出端
+            signal(SIGCHLD, SIG_DFL);          //设置子进程对SIGCHLD信号默认处理方式
+            signal(SIGPIPE, signalHandler);    //设置子进程对SIGPIPE信号捕捉处理方式
+            write(fds[1], data, strlen(data)); //子进程写data到管道
+        }
+        else if (pid > 0) //父进程
+        {
+            pid_t self = getpid(); //获取进程ID
+            printf("父进程pid:%d\n", self);
+            close(fds[0]); //关闭父进程管道读出端
+            wait(nullptr); //等待子进程执行完成
+        }
+    }
+    else
+    {
+        printf("创建管道失败\n");
+        exit(0);
+    }
+
     cout << endl;
+}
+
+/**
+ * @brief 信号处理函数
+ *
+ * @param sig 信号
+ */
+void signalHandler(int sig)
+{
+    printf("signalHandler begin ...\n");
+    switch (sig)
+    {
+    case SIGPIPE:
+        printf("捕捉到SIGPIPE信号,管道读出端不存在 ...\n");
+        break;
+    case SIGCHLD:
+        printf("捕捉到SIGCHLD信号,子进程存在 ...\n");
+        break;
+    default:
+        break;
+    }
 }
 
 void test2()
--- CodeStudy/ch07/example16/include/Global.h
+++ CodeStudy/ch07/example16/include/Global.h
@@ -4,6 +4,16 @@
 #include <iostream>
 #include <string>
 #include <memory>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <linux/limits.h>
+#include <sys/wait.h>
+#include <signal.h>
 
 using namespace std;
 
