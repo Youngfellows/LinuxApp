9140028da1df01681542c6295a6d78da866df112
Youngfellows <Youngfellows@163.com>
2022-10-13 16:10:37 +0800

    xxxx

--- CodeStudy/ch09/example11/Sample.cpp
+++ CodeStudy/ch09/example11/Sample.cpp
@@ -3,12 +3,28 @@
 void test1()
 {
     cout << "test1():: ..." << endl;
+    NetUtil::getIP();
     cout << endl;
 }
 
 void test2()
 {
     cout << "test2():: ..." << endl;
+    std::shared_ptr<TCPServer> tcpServer = std::make_shared<TCPServer>();
+    bool res = tcpServer->create(); //创建TCP服务端
+    if (res)
+    {
+        res = tcpServer->bindSocket(); //绑定本机网络地址信息
+        if (res)
+        {
+            res = tcpServer->listenSocket(); //监听TCP Socket套接字
+            if (res)
+            {
+                tcpServer->acceptSocket(); //等待客户端来连接服务端
+            }
+        }
+    }
+
     cout << endl;
 }
 
